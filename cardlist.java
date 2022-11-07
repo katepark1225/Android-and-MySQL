551176a8c94e3543767d6d579b2400a42013547a
katepark1225 <117127078+katepark1225@users.noreply.github.com>
2022-11-07 09:42:58 +0900

    Create cardlist.java

---
+++ cardlist.java
@@ -0,0 +1,28 @@
+package com.example.project;
+
+public class cardlist {
+
+    private String productName;
+    private String productSupplier;
+    private float productStock;
+
+    public String getProductName() {
+        return productName;
+    }
+    public String getProductSupplier() {
+        return productSupplier;
+    }
+    public float getProductStock() {
+        return productStock;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+    public void setProductSupplier(String productSupplier) {
+        this.productSupplier = productSupplier;
+    }
+    public void setProductStock(float productStock) {
+        this.productStock = productStock;
+    }
+}
